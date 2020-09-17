2c7cf78fcd3b85f7f080129d08700740fd43aaa0
jeongk469 <jeongk469@gmail.com>
2020-09-17 15:47:48 -0700

    passport facebook

--- authenticate.js
+++ authenticate.js
@@ -6,6 +6,8 @@ var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const Dishes = require('./models/dishes');
 var config = require('./config.js');
+var FacebookTokenStrategy = require('passport-facebook-token');
+
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -68,6 +70,33 @@ exports.verifyUser = passport.authenticate('jwt', {session: false});
         return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
   
 
     
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1001390723616033',
+        clientSecret: 'd3d6c8067830cdbb003bc0e5e3a476bd'
+    }
 }
